a88ecd44a623f5e30323bf1aa802962afff0ce18
EdgarFuentes100 <edgarfuentes139@gmail.com>
2026-02-15 04:57:24 -0800

    Force IPv4 for Gmail SMTP

--- services/enviarEmail.js
+++ services/enviarEmail.js
@@ -9,9 +9,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASS
   },
-  tls: {
-    rejectUnauthorized: false
-  }
+  family: 4 
 });
 
 /**
